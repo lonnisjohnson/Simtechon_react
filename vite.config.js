2dca72e9fbafa1c03da153b2820f0cc9207ef4a9
Lonnis <lonnisjohnson@gmail.com>
2026-07-21 22:33:07 +0530

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+
   server: {
     proxy: {
-      // Forward all /api/* requests to the Express backend
       '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
